85643ed812b3d8ef4b4249a5b35b1af042d04cd4
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-06-21 15:57:04 +0200

    Formatted code in example 2

--- examples/02-creating-a-function-from-a-folder/handler.py
+++ examples/02-creating-a-function-from-a-folder/handler.py
@@ -1,7 +1,9 @@
 from helper import square
 
+
 def foo():
     return "bar"
+
 
 def handle(data):
     # You can refer to functions outside the handle function
@@ -11,9 +13,5 @@ def handle(data):
     value = data["value"]
     squared_value = square(value)
     print(f"Square of {value} is {squared_value}")
-    
-    return {
-        "squaredValue": squared_value,
-        "value": value,
-        "foo": bar
-    }
+
+    return {"squaredValue": squared_value, "value": value, "foo": bar}
