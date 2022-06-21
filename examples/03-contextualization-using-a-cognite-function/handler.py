c87095b06272f53c583ea3ab16d0baff10297aae
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-06-21 15:57:44 +0200

    Formatted code in example 3

--- examples/03-contextualization-using-a-cognite-function/handler.py
+++ examples/03-contextualization-using-a-cognite-function/handler.py
@@ -1,65 +1,76 @@
 def handle(client, data, secrets):
-    
+
     # When deploying a function from a notebook like this, all imports must be performed inside the `handle` function.
     from cognite.experimental import CogniteClient
     from cognite.client.data_classes import TimeSeriesUpdate
     import time
-    
-    # The entity matcher suggests matches with a certain score. To achieve a reasonable result, this score must be adjusted. 
+
+    # The entity matcher suggests matches with a certain score. To achieve a reasonable result, this score must be adjusted.
     # The default value of 0.75 has been chosen by inspecting the outcome of this function, and may be different on data from other customers.
     good_match_threshold = data.get("good_match_threshold", 0.75)
-    
+
     # Create experimental SDK client as the contextualization API's are in playground and are thus not available in the regular SDK.
     client = CogniteClient(
-        base_url = client.config.base_url,
-        token   = secrets.get("token"),
-        project = client.config.project
+        base_url=client.config.base_url,
+        token=secrets.get("token"),
+        project=client.config.project,
     )
 
     # Download all assets and time series, using 5 requests in parallel
     assets = client.assets.list(limit=-1, partitions=5)
     time_series = client.time_series.list(limit=-1, partitions=5)
-    
+
     # Create simplified objects with only name and id
     assets_simplified = [{"id": asset.id, "name": asset.name} for asset in assets]
     time_series_simplified = [{"id": ts.id, "name": ts.name} for ts in time_series]
-
-    # Train the ML Entity Matcher on the data. 
+    print("Assets Length: ", len(assets_simplified))
+    print("Time Series Length: ", len(time_series_simplified))
+    # Train the ML Entity Matcher on the data.
     # The SDK expects as input the array of objects you match FROM (time series) and a list of what you match TO (assets)
     t0 = time.time()
-    model = client.entity_matching.fit(sources=time_series_simplified, targets=assets_simplified)
-    
+    model = client.entity_matching.fit(
+        sources=time_series_simplified, targets=assets_simplified
+    )
+
     print(f"Training entity matcher model with id {model} ...")
     model.wait_for_completion()
     t1 = time.time()
-    print(f"Model {model} trained on {len(assets_simplified)} assets and {len(time_series_simplified)} time series using {t1-t0} seconds")
+    print(
+        f"Model {model} trained on {len(assets_simplified)} assets and {len(time_series_simplified)} time series using {t1-t0} seconds"
+    )
 
-    # Use the ML Entity Matcher model to match the data. 
+    # Use the ML Entity Matcher model to match the data.
     # This model can be reused, so training is not necessary each time, but we do it for simplicity in this example.
     t0 = time.time()
     job = model.predict(time_series_simplified)
-    result = job.result # This will wait for completion
+    result = job.result  # This will wait for completion
     t1 = time.time()
-    print(f"Predict finished after {t1-t0} seconds on {len(time_series_simplified)} time series.")
-    
+    print(
+        f"Predict finished after {t1-t0} seconds on {len(time_series_simplified)} time series."
+    )
+
     # Filter out the best matches with the threshold specified in the input
-    
+
     keep_good_matches_fn = lambda match: match["score"] >= good_match_threshold
     good_match_count = 0
     time_series_updates = []
     for item in result["items"]:
-        match_from = item["source"] # Time series
-        matches = item["matches"]   # Suggested asset matches for the time series
+        match_from = item["source"]  # Time series
+        matches = item["matches"]  # Suggested asset matches for the time series
         good_matches = list(filter(keep_good_matches_fn, matches))
         if len(good_matches) > 0:
             good_match_count += 1
             best_match = good_matches[0]
-            time_series_updates.append(TimeSeriesUpdate(id=match_from["id"]).asset_id.set(best_match["target"]["id"]))
-    
+            time_series_updates.append(
+                TimeSeriesUpdate(id=match_from["id"]).asset_id.set(
+                    best_match["target"]["id"]
+                )
+            )
+
     dry_run = data.get("update_timeseries", False)
     if not dry_run:
-        client.time_series.update(time_series_updates) # uncomment to actually update the asset_id field
+        client.time_series.update(
+            time_series_updates
+        )  # uncomment to actually update the asset_id field
         print(f"Matched {good_match_count} time series to assets")
-    return {
-        "matches": good_match_count
-    }
+    return {"matches": good_match_count}
